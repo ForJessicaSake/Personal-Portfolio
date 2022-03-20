3e54962ae509809b6dc8f558189eebf9f385039b
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-03-20 01:00:15 +0100

    work in progress

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-const sections = document.querySelectorAll('section[id]')
-
-function scrollActive() {
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        let sectionId = current.getAttribute('id')
-
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.link a[href*=' + sectionId + ']').classList.add('active')
-        } else {
-            document.querySelector('.link a[href*=' + sectionId + ']').classList.remove('active')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
-
-
 /*============================== SCROLL REVEAL ANIMATION =================================*/
 const sr = ScrollReveal({
     origin: 'top',
     distance: '2em',
     duration: 2200,
     delay: 13,
-    //     reset: true
+    // reset: true
 });
 
 sr.reveal('.text, .imagecontainer, .professionalskills, .ptext', {});
