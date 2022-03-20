32e1328565ce06f270cdab52a0657613506cf51e
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-03-20 17:42:57 +0100

    this is a work in progress

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /*============================== SCROLL REVEAL ANIMATION =================================*/
-// const sr = ScrollReveal({
-//     origin: 'top',
-//     distance: '2em',
-//     duration: 2200,
-//     delay: 13,
-//     // reset: true
-// });
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '2em',
+    duration: 2200,
+    delay: 13,
+    // reset: true
+});
 
-// sr.reveal('.text, .imagecontainer, .professionalskills, .ptext', {});
-// sr.reveal('.avartar, .textcontainer, .skillimage', { delay: 300 });
-// sr.reveal('.socials', { interval: 200 });
-// sr.reveal('.box, .workimages, .form', { interval: 200 });
+sr.reveal('.text, .imagecontainer, .professionalskills, .ptext', {});
+sr.reveal('.avartar, .aboutsubtitle, .abouttext, .skillimage', { delay: 400 });
+sr.reveal('.socials', { interval: 200 });
+sr.reveal('.box, .workimages, .form', { interval: 200 });
